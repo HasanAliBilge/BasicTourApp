c91cadf93326b74c74af76c63cf202a936291c62
HasanAliBilge <hasanalibilge42@gmail.com>
2024-10-29 22:38:15 +0300

    Add project files.

---
+++ Ders14/Form1.Designer.cs
@@ -0,0 +1,474 @@
+﻿namespace Ders14
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            button7 = new Button();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            groupBox2 = new GroupBox();
+            radioButton2 = new RadioButton();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            radioButton1 = new RadioButton();
+            label5 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label4 = new Label();
+            label6 = new Label();
+            comboBox1 = new ComboBox();
+            groupBox3 = new GroupBox();
+            comboBox2 = new ComboBox();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            listBox3 = new ListBox();
+            colorDialog1 = new ColorDialog();
+            colorDialog2 = new ColorDialog();
+            colorDialog3 = new ColorDialog();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            listBox4 = new ListBox();
+            label10 = new Label();
+            label11 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Monotype Corsiva", 21F, FontStyle.Italic, GraphicsUnit.Point, 162);
+            label1.Location = new Point(479, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(290, 43);
+            label1.TabIndex = 0;
+            label1.Text = "Bursa-Konya Seyahat\r\n";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button7);
+            groupBox1.Controls.Add(button6);
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Font = new Font("Segoe Print", 9F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            groupBox1.Location = new Point(89, 104);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(169, 170);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Yolcu Dizilimi";
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Monotype Corsiva", 11F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            button7.Location = new Point(122, 119);
+            button7.Name = "button7";
+            button7.Size = new Size(31, 30);
+            button7.TabIndex = 3;
+            button7.Text = "6";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Monotype Corsiva", 11F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            button6.Location = new Point(71, 119);
+            button6.Name = "button6";
+            button6.Size = new Size(31, 30);
+            button6.TabIndex = 3;
+            button6.Text = "5";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Monotype Corsiva", 11F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            button5.Location = new Point(23, 119);
+            button5.Name = "button5";
+            button5.Size = new Size(31, 30);
+            button5.TabIndex = 3;
+            button5.Text = "4";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Monotype Corsiva", 11F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            button4.Location = new Point(71, 73);
+            button4.Name = "button4";
+            button4.Size = new Size(31, 30);
+            button4.TabIndex = 3;
+            button4.Text = "3";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Monotype Corsiva", 11F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            button3.Location = new Point(23, 73);
+            button3.Name = "button3";
+            button3.Size = new Size(31, 30);
+            button3.TabIndex = 3;
+            button3.Text = "2";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Monotype Corsiva", 11F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            button2.Location = new Point(122, 26);
+            button2.Name = "button2";
+            button2.Size = new Size(31, 30);
+            button2.TabIndex = 3;
+            button2.Text = "1";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_1;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Monotype Corsiva", 11F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            button1.Location = new Point(23, 26);
+            button1.Name = "button1";
+            button1.Size = new Size(31, 30);
+            button1.TabIndex = 2;
+            button1.Text = "0";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(radioButton2);
+            groupBox2.Controls.Add(textBox2);
+            groupBox2.Controls.Add(textBox1);
+            groupBox2.Controls.Add(radioButton1);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Font = new Font("Segoe Print", 9F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            groupBox2.Location = new Point(336, 118);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(394, 135);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Yolcu Bilgileri";
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(257, 96);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(81, 30);
+            radioButton2.TabIndex = 7;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Bayan";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(143, 63);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(238, 27);
+            textBox2.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(143, 27);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(238, 27);
+            textBox1.TabIndex = 6;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(180, 96);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(62, 30);
+            radioButton1.TabIndex = 6;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Bay";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(25, 98);
+            label5.Name = "label5";
+            label5.Size = new Size(74, 26);
+            label5.TabIndex = 4;
+            label5.Text = "Cinsiyet:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(25, 63);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 26);
+            label3.TabIndex = 4;
+            label3.Text = "Telefon:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(25, 30);
+            label2.Name = "label2";
+            label2.Size = new Size(98, 26);
+            label2.TabIndex = 3;
+            label2.Text = "Adı Soyadı:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe Print", 10F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label4.Location = new Point(6, 53);
+            label4.Name = "label4";
+            label4.Size = new Size(195, 30);
+            label4.TabIndex = 4;
+            label4.Text = "Varış Noktası Seçimi:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe Print", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label6.Location = new Point(72, 91);
+            label6.Name = "label6";
+            label6.Size = new Size(67, 35);
+            label6.TabIndex = 5;
+            label6.Text = "Saat:";
+            label6.Click += label6_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Konya", "Afyonkarahisar", "Kütahya", "İnegöl" });
+            comboBox1.Location = new Point(207, 52);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(154, 34);
+            comboBox1.TabIndex = 3;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(comboBox2);
+            groupBox3.Controls.Add(label6);
+            groupBox3.Controls.Add(comboBox1);
+            groupBox3.Controls.Add(label4);
+            groupBox3.Font = new Font("Segoe Print", 9F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            groupBox3.Location = new Point(798, 104);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(394, 149);
+            groupBox3.TabIndex = 6;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Seyahat Talep Bilgisi";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "10.00", "13.00", "17.00", "22.00", "1.00" });
+            comboBox2.Location = new Point(207, 92);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(154, 34);
+            comboBox2.TabIndex = 7;
+            // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("Monotype Corsiva", 11F, FontStyle.Italic, GraphicsUnit.Point, 162);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 21;
+            listBox1.Location = new Point(34, 440);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(294, 193);
+            listBox1.TabIndex = 7;
+            // 
+            // listBox2
+            // 
+            listBox2.Font = new Font("Monotype Corsiva", 11F, FontStyle.Italic, GraphicsUnit.Point, 162);
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 21;
+            listBox2.Location = new Point(351, 440);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(323, 193);
+            listBox2.TabIndex = 8;
+            listBox2.SelectedIndexChanged += listBox2_SelectedIndexChanged;
+            // 
+            // listBox3
+            // 
+            listBox3.Font = new Font("Monotype Corsiva", 11F, FontStyle.Italic, GraphicsUnit.Point, 162);
+            listBox3.FormattingEnabled = true;
+            listBox3.ItemHeight = 21;
+            listBox3.Location = new Point(704, 440);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(321, 193);
+            listBox3.TabIndex = 9;
+            // 
+            // colorDialog2
+            // 
+            colorDialog2.Color = Color.Gold;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Unispace", 11F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label7.Location = new Point(89, 398);
+            label7.Name = "label7";
+            label7.Size = new Size(190, 23);
+            label7.TabIndex = 10;
+            label7.Text = "Koltuk Numarası";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Unispace", 11F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label8.Location = new Point(415, 398);
+            label8.Name = "label8";
+            label8.Size = new Size(190, 23);
+            label8.TabIndex = 11;
+            label8.Text = "Yolcu Bilgileri";
+            label8.Click += label8_Click;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Unispace", 11F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label9.Location = new Point(728, 398);
+            label9.Name = "label9";
+            label9.Size = new Size(262, 23);
+            label9.TabIndex = 12;
+            label9.Text = "Seyahat Talep Bilgisi";
+            // 
+            // listBox4
+            // 
+            listBox4.Font = new Font("Monotype Corsiva", 11F, FontStyle.Italic, GraphicsUnit.Point, 162);
+            listBox4.FormattingEnabled = true;
+            listBox4.ItemHeight = 21;
+            listBox4.Location = new Point(1040, 440);
+            listBox4.Name = "listBox4";
+            listBox4.Size = new Size(152, 193);
+            listBox4.TabIndex = 13;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Unispace", 11F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label10.Location = new Point(1077, 398);
+            label10.Name = "label10";
+            label10.Size = new Size(70, 23);
+            label10.TabIndex = 14;
+            label10.Text = "Ücret";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Ink Free", 22F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label11.Location = new Point(336, 305);
+            label11.Name = "label11";
+            label11.Size = new Size(627, 46);
+            label11.TabIndex = 15;
+            label11.Text = "En son koltuk numarası seçilmelidir. .(!) ";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Turquoise;
+            ClientSize = new Size(1222, 667);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(listBox4);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(listBox3);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private GroupBox groupBox1;
+        private Button button1;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private GroupBox groupBox2;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private ComboBox comboBox1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private GroupBox groupBox3;
+        private ComboBox comboBox2;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private ListBox listBox3;
+        private ColorDialog colorDialog1;
+        private ColorDialog colorDialog2;
+        private ColorDialog colorDialog3;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private ListBox listBox4;
+        private Label label10;
+        private Label label11;
+    }
+}
---
+++ Ders14/Form1.cs
@@ -0,0 +1,220 @@
+namespace Ders14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        int a, b, toplam;
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(textBox1.Text);
+            listBox1.Items.Add(button2.Text);
+            listBox2.Items.Add(textBox2.Text);
+
+            if (radioButton1.Checked)
+            {
+                listBox2.Items.Add(radioButton1.Text);
+            }
+            else if (radioButton2.Checked)
+            {
+                listBox2.Items.Add(radioButton2.Text);
+            }
+            listBox3.Items.Add(comboBox1.Text);
+            listBox3.Items.Add(comboBox2.Text);
+            if (comboBox1.Text == "Konya")
+                listBox4.Items.Add("600 Türk Lirasý");
+            else if (comboBox1.Text == "Kütahya")
+                listBox4.Items.Add("350 Türk Lirasý");
+            else if (comboBox1.Text == "Afyonkarahisar")
+                listBox4.Items.Add("480 Türk Lirasý");
+            else if (comboBox1.Text == "Ýnegöl")
+                listBox4.Items.Add("200 Türk Lirasý");
+            listBox2.Items.Add("............................");
+            listBox1.Items.Add("............................");
+            listBox3.Items.Add("............................");
+            listBox4.Items.Add("...............");
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(textBox1.Text);
+            listBox1.Items.Add(button3.Text);
+            listBox2.Items.Add(textBox2.Text);
+
+            if (radioButton1.Checked)
+            {
+                listBox2.Items.Add(radioButton1.Text);
+            }
+            else if (radioButton2.Checked)
+            {
+                listBox2.Items.Add(radioButton2.Text);
+            }
+            listBox3.Items.Add(comboBox1.Text);
+            listBox3.Items.Add(comboBox2.Text);
+            if (comboBox1.Text == "Konya")
+                listBox4.Items.Add("600 Türk Lirasý");
+            else if (comboBox1.Text == "Kütahya")
+                listBox4.Items.Add("350 Türk Lirasý");
+            else if (comboBox1.Text == "Afyonkarahisar")
+                listBox4.Items.Add("480 Türk Lirasý");
+            else if (comboBox1.Text == "Ýnegöl")
+                listBox4.Items.Add("200 Türk Lirasý");
+            listBox2.Items.Add("............................");
+            listBox1.Items.Add("............................");
+            listBox3.Items.Add("............................");
+            listBox4.Items.Add("...............");
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            listBox2.Items.Add(textBox1.Text);
+            listBox1.Items.Add(button4.Text);
+            listBox2.Items.Add(textBox2.Text);
+
+            if (radioButton1.Checked)
+            {
+                listBox2.Items.Add(radioButton1.Text);
+            }
+            else if (radioButton2.Checked)
+            {
+                listBox2.Items.Add(radioButton2.Text);
+            }
+            listBox2.Items.Add("............................");
+            listBox1.Items.Add("............................");
+            listBox3.Items.Add("............................");
+            listBox4.Items.Add("...............");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            listBox2.Items.Add(textBox1.Text);
+            listBox1.Items.Add(button5.Text);
+            listBox2.Items.Add(textBox2.Text);
+
+            if (radioButton1.Checked)
+            {
+                listBox2.Items.Add(radioButton1.Text);
+            }
+            else if (radioButton2.Checked)
+            {
+                listBox2.Items.Add(radioButton2.Text);
+            }
+            listBox3.Items.Add(comboBox1.Text);
+            listBox3.Items.Add(comboBox2.Text);
+            if (comboBox1.Text == "Konya")
+                listBox4.Items.Add("600 Türk Lirasý");
+            else if (comboBox1.Text == "Kütahya")
+                listBox4.Items.Add("350 Türk Lirasý");
+            else if (comboBox1.Text == "Afyonkarahisar")
+                listBox4.Items.Add("480 Türk Lirasý");
+            else if (comboBox1.Text == "Ýnegöl")
+                listBox4.Items.Add("200 Türk Lirasý");
+            listBox2.Items.Add("............................");
+            listBox1.Items.Add("............................");
+            listBox3.Items.Add("............................");
+            listBox4.Items.Add("...............");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            listBox2.Items.Add(textBox1.Text);
+            listBox1.Items.Add(button6.Text);
+            listBox2.Items.Add(textBox2.Text);
+
+            if (radioButton1.Checked)
+            {
+                listBox2.Items.Add(radioButton1.Text);
+            }
+            else if (radioButton2.Checked)
+            {
+                listBox2.Items.Add(radioButton2.Text);
+            }
+            listBox3.Items.Add(comboBox1.Text);
+            listBox3.Items.Add(comboBox2.Text);
+            if (comboBox1.Text == "Konya")
+                listBox4.Items.Add("600 Türk Lirasý");
+            else if (comboBox1.Text == "Kütahya")
+                listBox4.Items.Add("350 Türk Lirasý");
+            else if (comboBox1.Text == "Afyonkarahisar")
+                listBox4.Items.Add("480 Türk Lirasý");
+            else if (comboBox1.Text == "Ýnegöl")
+                listBox4.Items.Add("200 Türk Lirasý");
+            listBox2.Items.Add("............................");
+            listBox1.Items.Add("............................");
+            listBox3.Items.Add("............................");
+            listBox4.Items.Add("...............");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            listBox2.Items.Add(textBox1.Text);
+            listBox1.Items.Add(button7.Text);
+            listBox2.Items.Add(textBox2.Text);
+
+            if (radioButton1.Checked)
+            {
+                listBox2.Items.Add(radioButton1.Text);
+            }
+            else if (radioButton2.Checked)
+            {
+                listBox2.Items.Add(radioButton2.Text);
+            }
+            listBox3.Items.Add(comboBox1.Text);
+            listBox3.Items.Add(comboBox2.Text);
+            if (comboBox1.Text == "Konya")
+                listBox4.Items.Add("600 Türk Lirasý");
+            else if (comboBox1.Text == "Kütahya")
+                listBox4.Items.Add("350 Türk Lirasý");
+            else if (comboBox1.Text == "Afyonkarahisar")
+                listBox4.Items.Add("480 Türk Lirasý");
+            else if (comboBox1.Text == "Ýnegöl")
+                listBox4.Items.Add("200 Türk Lirasý");
+            listBox2.Items.Add("............................");
+            listBox1.Items.Add("............................");
+            listBox3.Items.Add("............................");
+            listBox4.Items.Add("...............");
+        }
+    }
+}
